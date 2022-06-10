95c4e08bcee18c038f07b301ff2cdd6673a6f315
dfathu <dfathu@gmail.com>
2022-06-10 08:17:04 +0700

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dicoding_pertama_membuat_aplikasi_flutter/shared/responsive.dart';
+import 'package:dicoding_pertama_membuat_aplikasi_flutter/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,7 +11,47 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        if (Responsive.isMobile(context)) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                      top: defaultMargin,
+                      right: defaultMargin,
+                      left: defaultMargin),
+                  height: 25,
+                  width: 25,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/icons/home_icon.png'))),
+                ),
+              ],
+            ),
+          );
+        } else if (Responsive.isTablet(context)) {
+          return Text(
+            'NewsHome',
+            style: blackTextStyle.copyWith(
+                fontSize: 32, fontWeight: medium, letterSpacing: 2),
+          );
+        } else {
+          return Text(
+            'NewsHome',
+            style: blackTextStyle.copyWith(
+                fontSize: 32, fontWeight: medium, letterSpacing: 2),
+          );
+        }
+      })),
+    );
   }
 }
