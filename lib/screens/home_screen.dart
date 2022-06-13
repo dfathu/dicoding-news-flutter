ed524bb1a4dcca63a4e376b246aeb044d4f0d45a
fathu danur <fathudanur.prabaswara@gmail.com>
2022-06-13 11:37:10 +0700

    update list untuk menampillkan data dari jsonplaceholder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,18 +23,62 @@ class _HomeScreenState extends State<HomeScreen> {
         if (Responsive.isMobile(context)) {
           return SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  margin: EdgeInsets.only(
-                      top: defaultMargin,
-                      right: defaultMargin,
-                      left: defaultMargin),
-                  height: 25,
-                  width: 25,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/icons/home_icon.png'))),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                          top: defaultMargin, left: defaultMargin),
+                      height: 30,
+                      width: 30,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/icons/home_icon.png'))),
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.only(
+                          top: defaultMargin, right: defaultMargin),
+                      onPressed: () {},
+                      icon: const Icon(Icons.bookmark_border),
+                      iconSize: 30,
+                    )
+                  ],
                 ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ConstrainedBox(
+                              constraints: const BoxConstraints(
+                                minWidth: 200,
+                                minHeight: 200,
+                                maxWidth: 400,
+                                maxHeight: 400,
+                              ),
+                              child: Image.asset('assets/icons/home_icon.png',
+                                  fit: BoxFit.cover),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Expanded(
+                                flex: 1, child: Text("hohohohohohohhhoh"))
+                          ],
+                        ),
+                      );
+                    }),
               ],
             ),
           );
